48ba97b54ae813c720db0e2b30b5046574262de7
Neuro Nada Academy <powermovement.id@gmail.com>
2026-04-04 20:22:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import plotly.graph_objects as go
 
 # --- PENGATURAN HALAMAN ---
 st.set_page_config(
-    page_title="NLP Deep Analysis | Neuro Nada", 
+    page_title="Neuro Nada Deep Analysis", 
     page_icon="🧠", 
     layout="centered"
 )
